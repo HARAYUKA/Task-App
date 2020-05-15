653b4a02f27c7173a81980604dc41042eb328dd0
HARAYUKA <tukituki442@icloud.com>
2020-05-15 08:05:17 +0000

    C6終了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,30 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  
   def index
-    @user = User.find(params[:user_id])
-    @tasks = @user.tasks
+    @tasks = @user.tasks.order(created_at: :desc)
   end
   
   def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = @user.tasks.build(task_params)
+    @task.save
+    redirect_to user_tasks_url
   end
   
   def show
   end
+  
+  private
+  
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
+  
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
