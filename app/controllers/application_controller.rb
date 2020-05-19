c2df4a6bc1529fc21d949e983ccdbee06024bc9e
HARAYUKA <tukituki442@icloud.com>
2020-05-19 23:13:53 +0000

    E6終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
-  # 原ログインユーザーであるか
+  # 現ログインユーザーであるか
   def correct_user
     redirect_to root_url unless current_user?(@user)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
   before_action :logged_in_user, only: %i(index show edit update destroy)
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: %i(index destroy)
   before_action :correct_user, only: %i(edit update)
+  before_action :admin_or_correct_user, only: %i(show)
   
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
@@ -56,7 +57,6 @@ class UsersController < ApplicationController
     
     def logged_in_user
       unless logged_in?
-        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
@@ -64,5 +64,14 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to root_url unless current_user.admin?
+    end
+    
+    # 管理権限者かログインしているユーザーか
+    def admin_or_correct_user
+      @user = User.find(params[:id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "権限がありません。"
+        redirect_to root_url
+      end
     end
 end
