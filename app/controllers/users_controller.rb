17cbcd6db6813d2b26dcbf8678cb96bfdbf7c4c1
HARAYUKA <tukituki442@icloud.com>
2020-05-19 15:20:11 +0000

    E3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
   before_action :logged_in_user, only: %i(index show edit update destroy)
   before_action :admin_user, only: :destroy
+  before_action :correct_user, only: %i(edit update)
   
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
