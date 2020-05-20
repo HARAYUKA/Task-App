b4f3fd509ab829f226a92148a3c5ab9e73acc1be
HARAYUKA <tukituki442@icloud.com>
2020-05-20 14:20:50 +0000

    E7終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,23 @@ class ApplicationController < ActionController::Base
   def correct_user
     redirect_to root_url unless current_user?(@user)
   end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+  
+  # 管理権限者かログインしているユーザーか
+  def admin_or_correct_user
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "権限がありません。"
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,24 +54,4 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-    
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-    
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
-    
-    # 管理権限者かログインしているユーザーか
-    def admin_or_correct_user
-      @user = User.find(params[:id]) if @user.blank?
-      unless current_user?(@user) || current_user.admin?
-        flash[:danger] = "権限がありません。"
-        redirect_to root_url
-      end
-    end
 end
