0c9e0efff56dfc057cf4da932f968d78d68c6eec
HARAYUKA <tukituki442@icloud.com>
2020-05-24 06:38:05 +0000

    最終確認終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
-  # 現ログインユーザーであるか
-  def correct_user
-    redirect_to root_url unless current_user?(@user)
-  end
-  
   def logged_in_user
     unless logged_in?
       flash[:danger] = "ログインしてください。"
       redirect_to login_url
     end
+  end
+  
+  # 現ログインユーザーであるか
+  def correct_user
+    redirect_to root_url unless current_user?(@user)
   end
   
   def admin_user
